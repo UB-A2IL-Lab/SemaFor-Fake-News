06bd58a41d0e14a61832187f84d1b70d89f2a09b
yjzhu <yjzhu@buffalo.edu>
2021-02-23 23:26:15 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,67 @@
+
+    
+# Dependencies
+
+```
+Python=3.6
+Pytorch=1.2.0
+spacy=2.0.12
+transformers=4.2.2 (pip install pytorch-transformers)
+```
+
+# Original NeuralNews Dataset
+
+Please follow the instructions here (https://cs-people.bu.edu/rxtan/projects/didan/) to download the NeuralNews dataset. In particular, download this file (https://drive.google.com/file/d/1rswGdNNfl4HoP9trslP0RUrcmSbg1_RD/view?usp=sharing) and place it into the data folder.
+
+# Preprocess Data (or Download)
+
+### Image Features 
+For each image, we extract 36 region features using a Faster-RCNN model (https://github.com/peteanderson80/bottom-up-attention) that is pretrained on Visual Genome. The region features for each image is stored separately as a .npy file.
+
+### Language Features
+To convert the articles and captions into the required input format, please go to https://github.com/nlpyang/PreSumm/blob/master/README.md and carry out steps 3 to 5 of data preparation.
+
+- Step 3: Remember to change '.story' to '.txt' in tokenize() in data_builder.py.
+```
+python preprocess.py -mode tokenize -raw_path /mnt/data/NeuralNews/data/real_caps/txt -save_path /mnt/data/NeuralNews/data/real_caps/tokenized
+```
+
+- Step 4: 1)../urls are included in the codebase. But we do not need it for a customized dataset because it's only for data spliting. 2)Pay attention to L347-348 in data_builder.py. 3) Chnage shard_size=2000 in preprocess.py to save all the tokenized jsons in to one file.
+```
+python preprocess.py -mode format_to_lines -raw_path /mnt/data/NeuralNews/data/real_arts/tokenized -save_path /mnt/data/NeuralNews/data/real_arts/json_data -n_cpus 1 -use_bert_basic_tokenizer false
+```
+
+- Step 5: Rember to change some arguments.
+```
+'-min_src_nsents', default=1
+'-min_src_ntokens_per_sent', default=1
+'-min_tgt_ntokens', default=0
+
+python preprocess.py -mode format_to_bert -raw_path /mnt/data/NeuralNews/data/real_arts/json_data -save_path /mnt/data/NeuralNews/data/real_arts/bert_data -lower -n_cpus 1 -log_file ../logs/preprocess.log
+```
+
+### Named Entities
+We use the SpaCY python library to parse the articles and captions to detect named entities. We store this information as dictionary where the keys are the article names and the values are sets of detected name entities.
+
+# Required Arguments
+
+1. captioning_dataset_path: Path to GoodNews captioning dataset json file
+2. fake_articles: Path to generated articles
+3. image_representations_dir: Directory which contains the object representations of images
+4. real_articles_dir: Directory which contains the preprocessed Torch text files for real articles
+5. fake_articles_dir: Directory which contains the preprocessed Torch text files for generated articles
+6. real_captions_dir: Directory which contains the preprocessed Torch text files for real captions
+7. ner_dir: Directory which contains a dictionary of named entities for each article and caption
+8. model_dir: Directory which contains the pre-trtained models
+9. test_with: Testing set selecting from 'fake', 'real' and 'fake-real'.
+10. is_train: Training from scratch or not.
+
+# Training
+```
+CUDA_VISIBLE_DEVICES=0 python train.py -num_workers 4 -test_with fake-real -is_train True
+```
+
+# Testing
+```
+CUDA_VISIBLE_DEVICES=0 python train.py -num_workers 4 -test_with fake-real -is_train False
+```
---
+++ dataloader.py
@@ -0,0 +1,212 @@
+import os
+import sys
+import torch
+import torch.nn.functional as F
+import numpy as np
+import pickle
+import json
+import random
+import math
+import bisect
+from torch.utils.data import Dataset
+import ipdb
+
+class Loader(Dataset):
+    def __init__(self, args, split, captioning_dataset, art2id, fake_articles):
+        self.args = args
+        self.split = split
+        self.img_feats_dir = args.image_representations_dir
+        self.real_arts_dir = args.real_articles_dir
+        self.fake_arts_dir = args.fake_articles_dir
+        self.real_caps_dir = args.real_captions_dir
+        self.real_arts = torch.load(os.path.join(self.real_arts_dir, split + '.bert.pt'))
+        self.fake_arts = torch.load(os.path.join(self.fake_arts_dir, split + '.bert.pt'))
+        self.real_caps = torch.load(os.path.join(self.real_caps_dir, split + '.bert.pt'))
+   
+        self.ner_dir = args.ner_dir
+
+        self.captioning_dataset = captioning_dataset
+        self.art2id = art2id
+        self.fake_articles = fake_articles
+
+        # Binary named entities
+        self.realarts2id, self.caps2id, self.arts2caps, self.fakearts2id = self.parse()
+        self.arts = []
+        # for i in self.realarts2id:
+        #     name = '1_' + i
+        #     self.arts.append(name)
+
+        # # Why are fake artickles doubled for training?
+        # # Not really. There is a return.
+        # if split == 'train':
+        #     neg_count = 0
+        #     for i in self.fakearts2id:
+        #         name = '0_' + i
+        #         self.arts.append(name)
+        #         neg_count += 1
+        #     return
+
+            
+
+        # There are only fake articles when testing and validating, but with real
+        # ones when training. 
+        if split == 'train' or args.test_with == 'fake-real':
+            for i in self.realarts2id:
+                name = '1_' + i
+                self.arts.append(name)
+            for i in self.fakearts2id:
+                name = '0_' + i
+                self.arts.append(name)
+        else:
+            if args.test_with == 'fake':
+                for i in self.fakearts2id:
+                    name = '0_' + i
+                    self.arts.append(name)
+            elif args.test_with == 'real':
+                for i in self.realarts2id:
+                    name = '1_' + i
+                    self.arts.append(name)
+        # ipdb.set_trace()
+
+    def parse(self):
+        # realarts2id = {k: '580d2f8595d0e022439c4c3f', v: 258XX}
+        realarts2id = {}
+        for i, d in enumerate(self.real_arts):
+            name = d['name']
+            realarts2id[name] = i
+
+        # caps2id = {k: '580d2f8595d0e022439c4c3f_0', v: 567xx}
+        # arts2caps = {k: '580d2f8595d0e022439c4c3f', v: ['580d2f8595d0e022439c4c3f_0', 
+        #                                                   '580d2f8595d0e022439c4c3f_1']}
+        caps2id = {}
+        arts2caps = {}
+        for i, d in enumerate(self.real_caps):
+            name = d['name']
+            art = name.split('_')[0]
+            if art not in arts2caps:
+                arts2caps[art] = []
+            arts2caps[art].append(name)
+            caps2id[name] = i
+         
+        
+        fakearts2id = {}
+        for i, d in enumerate(self.fake_arts):
+            name = d['name']
+            fakearts2id[name] = i
+
+        # delete items whose values are None
+        # 2021-02-09
+        # ipdb.set_trace()
+        cap_keys = set(arts2caps.keys())
+        real_keys = set(realarts2id.keys())
+        fake_keys = set(fakearts2id.keys())
+        common_keys = cap_keys.intersection(real_keys, fake_keys)
+        c_realarts2id = {} 
+        c_arts2caps = {}
+        c_fakearts2id = {}
+        for k in common_keys:
+            c_realarts2id[k] = realarts2id[k]
+            c_arts2caps[k] = arts2caps[k]
+            c_fakearts2id[k] = fakearts2id[k]
+
+
+        return c_realarts2id, caps2id, c_arts2caps, c_fakearts2id
+
+    def _pad(self, data, pad_id, width=-1):
+        if (width == -1):
+            width = max(len(d) for d in data)
+        rtn_data = [d + [pad_id] * (width - len(d)) for d in data]
+        return rtn_data
+
+    def preprocess(self, ex):
+        src = ex['src']
+        tgt = ex['tgt'][:self.args.max_tgt_len][:-1]+[2]
+        src_sent_labels = ex['src_sent_labels']
+        segs = ex['segs']
+        if(not self.args.use_interval):
+            segs=[0]*len(segs)
+        clss = ex['clss']
+        src_txt = ex['src_txt']
+        tgt_txt = ex['tgt_txt']
+
+        end_id = [src[-1]]
+
+        tmp = src[:-1][:self.args.max_pos - 1] + end_id
+
+        src = src[:-1][:self.args.max_pos - 1] + end_id
+        segs = segs[:self.args.max_pos]
+        max_sent_id = bisect.bisect_left(clss, self.args.max_pos)
+        src_sent_labels = src_sent_labels[:max_sent_id]
+        clss = clss[:max_sent_id]
+
+        return src, tgt, segs, clss, src_sent_labels
+
+    def __getitem__(self, index):
+        # Gets real article
+        art = self.arts[index]
+        label = int(art.split('_')[0])
+        art_id = art.split('_')[-1]
+
+        # img_dict = self.captioning_dataset[art_id]['images']
+        # ipdb.set_trace()
+        
+        if label == 1:
+            idx = self.realarts2id[art_id]
+            art = self.real_arts[idx]
+            art = self.preprocess(art)
+            art_path = os.path.join(self.ner_dir, '1_' + art_id + '.pkl')
+            art_ner = pickle.load(open(art_path, 'rb'))
+        else:
+            idx = self.fakearts2id[art_id]
+            art = self.fake_arts[idx]
+            art = self.preprocess(art)
+            art_path = os.path.join(self.ner_dir, '0_' + art_id + '.pkl')
+            art_ner = pickle.load(open(art_path, 'rb'))
+    
+        # Get images and captions
+        imgs = self.arts2caps[art_id]
+        if len(imgs) > 3:
+            imgs = imgs[:3]
+        combined_feats = []
+        combined_caps = []
+        cap_text = []
+        # didan_dataloader.py L130-133
+        for i in imgs:
+            cap_path = os.path.join(self.ner_dir, i + '.pkl')
+            cap_ner = pickle.load(open(cap_path, 'rb'))
+            cap_text.append(cap_ner)   
+
+            feat_path = os.path.join(self.img_feats_dir, i + '.npy')
+            feat = torch.from_numpy(np.load(feat_path))
+            combined_feats.append(feat.unsqueeze(0))
+
+            cap_idx = self.caps2id[i]
+            cap = self.real_caps[cap_idx]
+            tmp = self.preprocess(cap)
+            combined_caps.append(tmp)
+
+        num_imgs = len(imgs)
+        if num_imgs < 3:
+            for r in range(3 - num_imgs):
+                combined_feats.append(torch.zeros(1, 36, 2048))
+                combined_caps.append(combined_caps[0])
+                cap_text.append(set())
+        img_exists = torch.zeros(3, dtype=torch.bool)
+        for i in range(num_imgs):
+            img_exists[i] = True
+        combined_feats = torch.cat(combined_feats, dim=0)
+        
+        combine = list(art)
+        combine.append(combined_feats)
+        combine.append(img_exists)
+        combine.append(combined_caps)
+        # combine.append(torch.tensor(label, dtype=torch.bool))
+        combine.append(torch.tensor(label))
+        combine.append(art_ner)
+        combine.append(cap_text)
+        combine = tuple(combine)
+
+        return combine
+
+    def __len__(self):
+        return len(self.arts)
