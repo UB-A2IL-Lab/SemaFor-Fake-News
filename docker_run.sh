41f52e37cb25f122017f96b89897ea7edbf87351
yjzhu <yjzhu@buffalo.edu>
2021-03-21 13:14:35 -0400

    soft link a directory of the local machine to docker

--- docker_run.sh
+++ docker_run.sh
@@ -1,7 +1,11 @@
-# # load docker image
+# # load docker image from local machine
 # sudo docker load -i docker_image.tar
+# # pull docker image from docker hub
+# docker pull yjzhux/image-text:1.0-neuralnews-test
 
 # run docker
+# make sure you've doloaded all the required data and map the path to the folder
+# of the docker (/app/data)
 sudo docker run -v /mnt/data/NeuralNews/data/:/app/data \
                 -it --rm --gpus all \
-                ub/image-text:v1
+                ub/image-text:v1.0
